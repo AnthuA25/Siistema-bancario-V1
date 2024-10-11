0b071fc63b6b13894577f605401732670a9043fb
Barbara <0deabreubarbara@gmail.com>
2024-10-11 16:48:55 -0500

    feat: adding routes names

--- backEnd/src/routes/index.ts
+++ backEnd/src/routes/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import transactions from './transactionRoutes';
 import usersRouter from './userRouters';
+import accountsRouter from './accountRoutes';
 
 const router = express.Router();
 
-router.use('/', transactions);
+router.use('/transactions', transactions);
+router.use('/account', accountsRouter)
 router.use('/',usersRouter);
 
 export default router;
