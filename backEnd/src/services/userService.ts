231836af2e02799bf6c869957f006cd9cd8cc31f
Aylin <aylin_santacruz@hotmail.com>
2024-10-11 10:11:14 -0500

    feat:Add bcrypt to createUser

--- backEnd/src/services/userService.ts
+++ backEnd/src/services/userService.ts
@@ -1,5 +1,5 @@
 import User  from "../models/User";
-
+import bcrypt from "bcrypt";
 
 export const seeUsers = async () => {
     const users = await User.findAll(); 
@@ -8,10 +8,21 @@ export const seeUsers = async () => {
 
 export const createUserService = async (username: string, password: string) => {
     try {
-        const newUser = await User.create({ username, password });
+        const hashedPassword = await bcrypt.hash(password, 10 );
+        const newUser = await User.create({ username, password:hashedPassword });
         return newUser;
     } catch (error) {
+        console.log(error)
         throw new Error("Error creating user");
+    }
+};
+
+export const userById = async(username:string) =>{
+    try {
+        const userId = await User.findOne({where:{username:username}})
+        return userId;
+    } catch (error) {
+        throw new Error("Errorwhen searching for id");
     }
 };
 
